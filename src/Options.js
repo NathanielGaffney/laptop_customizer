3db172df6be5989f77a4f97fc9c01f4da4911162
NathanielGaffney <nate440@gmail.com>
2020-09-01 21:38:48 -0400

    breakout featureitem

--- src/Options.js
+++ src/Options.js
@@ -9,21 +9,6 @@ export default function Options(props) {
         const options = props.features[feature].map(item => {
             const itemHash = slugify(JSON.stringify(item));
                 return <FeatureItem itemHash={itemHash} item={item} feature={feature} selected={props.selected} updateFeature={props.updateFeature}/>;
-            // return (
-            //     <div key={itemHash} className="feature__item">
-            //         <input
-            //             type="radio"
-            //             id={itemHash}
-            //             className="feature__option"
-            //             name={slugify(feature)}
-            //             checked={item.name === props.selected[feature].name}
-            //             onChange={e => props.updateFeature(feature, item)}
-            //         />
-            //         <label htmlFor={itemHash} className="feature__label">
-            //             {item.name} ({USCurrencyFormat.format(item.cost)})
-            //   </label>
-            //     </div>
-            // )
         });
 
         return (
